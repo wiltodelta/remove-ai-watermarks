d9204e2e1fec6f4f945702711d1c76feeeb2fcaa
Victor Kuznetsov <kuznetsov.va@gmail.com>
2026-07-13 12:27:06 +0300

    chore(release): bump version to 0.15.2

--- src/remove_ai_watermarks/__init__.py
+++ src/remove_ai_watermarks/__init__.py
@@ -25,7 +25,7 @@ _os.environ.setdefault("TRANSFORMERS_VERBOSITY", "error")
 _warnings.filterwarnings("ignore", message=r".*ImageProcessorFast.*")
 
 
-__version__ = "0.15.1"
+__version__ = "0.15.2"
 
 __all__ = ["__version__", "remove_visible", "visible_provenance"]
 
